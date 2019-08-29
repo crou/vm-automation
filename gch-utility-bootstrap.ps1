371d0ad836053cc3ee050f62d77534dc6e6e9e51
Christian Rousseau <crousseau@crou.net>
2019-08-28 23:40:16 -0400

    monitoring and restart vpn

--- gch-utility-bootstrap.ps1
+++ gch-utility-bootstrap.ps1
@@ -1,11 +1,11 @@
 $session = (query session $env:USERNAME)
 if ($session -match '^>console') {
-    Start-Transcript -Path $PSScriptRoot\bootstrap.log -Append -ErrorAction SilentlyContinue
-    write-output "Running in Console"
+    Start-Transcript -Path $PSScriptRoot\bootstrap.log -ErrorAction SilentlyContinue
+    write-output "$(Get-Date -Format "o")`tRunning in Console"
 }
 else {
-    write-output "Running in RDP. Exiting."
-    return
+    write-output "$(Get-Date -Format "o")`tRunning in RDP. Exiting."
+    #    return
 }
 
 # Retrieve info from the azure metadata service
@@ -37,6 +37,134 @@ function Get-Tags {
     return $dictionary
 }
 
+function ConvertTo-Metric {
+    param(
+        [Parameter(Mandatory)]
+        $Name,
+        [Parameter(Mandatory)]
+        $Namespace,
+        [Parameter(ValueFromPipeline)]
+        $SingleValue,
+        $Min,
+        $Max,
+        $Sum,
+        $Count,
+        $Timestamp = [DateTime]::Now.ToUniversalTime().ToString("o")
+    )
+    if ($SingleValue -ne $null) {
+        $Min = $SingleValue
+        $Max = $SingleValue
+        $Sum = $SingleValue
+        $Count = 1
+    }
+    $metric = @{
+        time = $Timestamp
+        data = @{
+            baseData = @{
+                metric    = "$Name"
+                namespace = "$Namespace"
+                series    = @(
+                    @{ 
+                        min   = $min 
+                        max   = $max 
+                        sum   = $sum
+                        count = $count
+                    } 
+                )
+            }
+        }
+    } | convertto-json -depth 10
+    return $metric
+}
+Function Send-Webhook {
+    param(
+        $uri,
+        $message
+    )
+
+    $Body = @{
+        'text' = $message
+    }
+
+    $params = @{
+        Headers = @{'accept' = 'application/json' }
+        Body    = $Body | convertto-json
+        Method  = 'Post'
+        URI     = $uri
+    }
+
+    try {
+        $null = Invoke-RestMethod @params
+    }
+    catch {
+        Write-Warning "Unable to send the webhook"
+    }
+}
+function  Send-AzCustomMetric {
+    param (
+        [Parameter(ValueFromPipeline)]
+        $data
+    )
+
+    # Get a monitor Token if not expired
+    if ($script:monToken -eq $null -or ([datetime]'1/1/1970').AddSeconds($script:monToken.expires_on) -lt (get-date)) {
+        $script:monToken = Get-Metadata -Path '/identity/oauth2/token?api-version=2018-02-01&resource=https://monitoring.azure.com/'
+    }   
+    
+    if ($script:monUrl -eq $Null) {    
+        $script:monUrl = "https://$($instance.compute.location).monitoring.azure.com$($instance.compute.resourceId)/metrics"
+    }
+    #Write-Output $url
+    
+    $null = Invoke-WebRequest -UseBasicParsing -Uri $script:monUrl -Method Post -Headers @{Authorization = "Bearer $($script:monToken.access_token)" } -Body $data -ContentType 'application/json'    -ErrorAction SilentlyContinue
+}
+
+function Get-RdpSession {
+    $pattern = '^(?<Interactive>[ >])(?<SessionName>\S*)\s+(?<UserName>\S*)\s+(?<ID>\d+)\s+(?<State>\S*)\s*(?:(?<Type>\S*)\s+(?<Device>\S*))?'
+    $Result = qwinsta.exe 2>$null
+    $Result | Where-Object { $_ -match $pattern } |
+    ForEach-Object {
+        $Properties = @{
+            ComputerName  = $Computer
+            IsInteractive = $matches.Interactive -as [bool]
+            SessionName   = $matches.SessionName
+            UserName      = $matches.UserName
+            ID            = [int]$matches.ID
+            State         = $matches.State
+            Type          = $matches.Type
+            Device        = $matches.Device
+        }
+        if ($PSVersion -eq 2) {
+            New-Object -TypeName PSObject -Property $Properties
+        }
+        else {
+            [PSCustomObject]$Properties
+        }
+    }
+}
+
+Function Invoke-VPNMonitor {
+    while ($true) {
+        $statusPre = $statusNow    
+        $statusNow = Get-NetAdapter -InterfaceDescription "Cisco AnyConnect Secure Mobility*" | Select-Object -ExpandProperty Status
+        if ($statusNow -ne 'up') {
+            Write-Output "$(Get-Date -Format "o")`tVPN Not Connected. Retry..."
+            0 | ConvertTo-Metric -Name "VPN Connected" -Namespace "Custom Metric" | Send-AzCustomMetric
+            Invoke-VPNConnect -VpnHost 'CSG.TICKETMASTER.COM' -Username $user -Password $passClear
+        }
+        elseif ($statusPre -ne 'up' -and $statusNow -eq 'up') {
+            Write-Output "$(Get-Date -Format "o")`tVPN is now successfully connected"
+            1 | ConvertTo-Metric -Name "VPN Connected" -Namespace "Custom Metric" | Send-AzCustomMetric
+            Send-Webhook -message "VPN on $env:computername is now Connected" -uri $webhookUri
+        }
+        elseif ($statusNow -eq 'up') {
+            write-output "$(Get-Date -Format "o")`tPing!"
+            1 | ConvertTo-Metric -Name "VPN Connected" -Namespace "Custom Metric" | Send-AzCustomMetric
+        }
+        Start-Sleep -Seconds 30
+    }
+}
+
 
 # Start the vpncli.exe and pass the username and password as input
 Function Invoke-VPNConnect {
@@ -48,8 +176,24 @@ Function Invoke-VPNConnect {
     )
     # Stop past client
     stop-process -Name "vpnui" -ErrorAction SilentlyContinue -Force
+    stop-process -Name "vpncli" -ErrorAction SilentlyContinue -Force
 
-    write-output "`nStart VPN Client"
+    # kill RDP Session
+    try {
+        Get-RdpSession | Where-Object { $_.State -eq 'active' -and $_.SessionName -like 'rdp*' -and $_.UserName -ne $env:username } | ForEach-Object { 
+            write-output "$(Get-Date -Format "o")`tKilling RDP Session $($_.id)"
+            start-process "rwinsta" -ArgumentList "$($_.id)" -Verb runas -ErrorAction SilentlyContinue
+        }
+    }
+    catch { Write-Warning "Unable to kill rdp session: $($Error[0].Exception.Message)" }
+
+    Get-RdpSession | Where-Object { $_.State -eq 'active' -and $_.SessionName -like 'rdp*' -and $_.UserName -eq $env:username } | ForEach-Object { 
+        write-output "$(Get-Date -Format "o")`tReconnecting RDP Session $($_.id) To Console"
+        start-process "tscon"  -ArgumentList "$($_.id) /dest:console" -Verb runas -ErrorAction SilentlyContinue
+    }
+  
+
+    write-output "$(Get-Date -Format "o")`tStart VPN Client"
     Start-Process -FilePath $VpncliPath -ArgumentList "connect $VpnHost" #-RedirectStandardOutput $psscriptroot\stdout.txt -RedirectStandardError $psscriptroot\stderr.txt
     $counter = 0; $h = 0;
     while ($counter++ -lt 1000 -and $h -eq 0) {
@@ -58,19 +202,18 @@ Function Invoke-VPNConnect {
     }
     #if it takes more than 10 seconds then display message
     if ($h -eq 0) {
-        Write-Output "Could not start the VPN it takes too long."
+        Write-Output "$(Get-Date -Format "o")`tCould not start the VPN it takes too long."
     }
     else {
         # make sure the vpncli is on foreground for the credential input (sendkey)
         [void] [Win]::SetForegroundWindow($h)
 
-        Write-Output "`t...and send user and pass"
+        Write-Output "$(Get-Date -Format "o")`t...and send user and pass"
         #Write login and password
         [System.Windows.Forms.SendKeys]::SendWait("$Username{Enter}")
         [System.Windows.Forms.SendKeys]::SendWait("$Password{Enter}")
 
     }
-    write-output "Done`n"
 }
 
 #Set foreground window function
@@ -90,10 +233,10 @@ Add-Type -AssemblyName System.Windows.Forms -ErrorAction Stop
 # Read vm instance context / metadata
 $instance = Get-Metadata
 $tags = Get-Tags $instance.compute.tags
-
+$webhookUri = 'https://outlook.office.com/webhook/dd61eade-0727-48f2-8025-c032bc8311d6@944dad0f-d51b-4889-bd6e-da46032d3301/IncomingWebhook/a8f324f51beb4f64a1d5023e4fb9cbc6/6f5b9aa7-8a39-487d-bf03-ef6af6890b98'
 
 # Get the Azure Vault token
-write-output "Read Key Vault"
+write-output "$(Get-Date -Format "o")`tRead Key Vault"
 $kv = Get-Metadata -Path '/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net'
 $kvToken = $kv.access_token
 $kvUrl = "$($tags['environment'])-kv-$($tags['namespace']).vault.azure.net"
@@ -105,17 +248,17 @@ $content = (Invoke-WebRequest -UseBasicParsing -Uri https://$kvUrl/secrets/$($kv
 $passClear = $content.value
 $pass = $content.value | ConvertTo-SecureString -AsPlainText -Force
 
+Write-Output "$(Get-Date -Format "o")`tVPN User is: $user"
+
 # install the cert if not there
 $cert = Get-ChildItem Cert:\CurrentUser\my | Where-Object Subject -like "*$user*"
 if ($cert -eq $null) {
-    write-output "Install the certificat for $user"
+    write-output "$(Get-Date -Format "o")`tInstall the certificat for $user"
     $certUrl = "https://$($tags['environment'])stinfraprovision.blob.core.windows.net/certs/$($user).pfx"
     $null = Invoke-WebRequest -UseBasicParsing -Uri $certUrl -OutFile "$env:TEMP\$($user).pfx"
     $null = Import-PfxCertificate -Password $pass -FilePath "$env:TEMP\$($user).pfx" -CertStoreLocation Cert:\CurrentUser\My
 }
 
 # Start the VPN
-Invoke-VPNConnect -VpnHost 'CSG.TICKETMASTER.COM' -Username $user -Password $passClear
-
-
+Invoke-VPNMonitor
 Stop-Transcript -ErrorAction SilentlyContinue
